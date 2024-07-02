64824db5332685cb90821ff6a2e4e64b07943e33
Hilmi Muharromi <hilmi@mtn-fm.id>
2024-07-02 16:43:59 +0700

    setup terragorm

---
+++ main.tf
@@ -0,0 +1,51 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = var.key-name
+  public_key = file("/home/hilmi/miro-dev-aws.pub")
+}
+
+
+# Creating security group to restrict/allow inbound connectivity
+resource "aws_security_group" "network-security-group" {
+  name        = var.network-security-group-name
+  description = "Open 22,443,80,8080"
+
+  # Define a single ingress rule to allow traffic on all specified ports
+  ingress = [
+    for port in [22, 80, 443, 8080] : {
+      description      = "TLS from VPC"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Jenkins-sg"
+  }
+}
+
+
+# Creating Ubuntu EC2 instance
+resource "aws_instance" "ubuntu-vm-instance" {
+  ami             = var.ubuntu-ami
+  instance_type   = var.ubuntu-instance-type
+  key_name        = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.network-security-group.id]
+    user_data              = templatefile("./install_jenkins.sh", {})
+
+  tags = {
+    Name = "jenkins-vm"
+  } 
+}
